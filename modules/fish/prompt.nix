33f2454aca48aed689fcf69d1edb9cde4e22657a
LunaCOLON3 <lunacolon3@proton.me>
2025-06-22 19:12:43 -0700

    make fish apply theme using bash

--- modules/fish/prompt.nix
+++ modules/fish/prompt.nix
@@ -6,10 +6,10 @@ let
   };
 in
 ''
-  source ${theme}
+  bash ${theme}
 
   # See https://github.com/tomyun/base16-fish/issues/7 for why this condition exists
   if status --is-interactive && test -z "$TMUX"
-    base16-${colors.slug}
+    base24-${colors.slug}
   end
 ''
