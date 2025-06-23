ae578e509951c21b1ba1c24dfe6e49c3def6b9c9
LunaCOLON3 <lunacolon3@proton.me>
2025-06-22 22:23:47 -0700

    Revert "exec bash"
    This reverts commit 85e3bef27ca0451b35380b477ff2d5731ab43cea.

--- modules/fish/prompt.nix
+++ modules/fish/prompt.nix
@@ -6,7 +6,7 @@ let
   };
 in
 ''
-  exec bash ${theme}
+  bash ${theme}
 
   # See https://github.com/tomyun/base16-fish/issues/7 for why this condition exists
   if status --is-interactive && test -z "$TMUX"
